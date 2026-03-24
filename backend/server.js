9140da6f57653c0a34d7705db3fa94ccf6473fd2
Ruqaya <ruqayaalasoomii@gmail.com>
2026-03-24 19:22:32 +0300

    t

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,8 @@ app.use(
       "http://127.0.0.1:3000",
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "https://scholarship-evaluation.vercel.app/",
+
     ],
     methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
